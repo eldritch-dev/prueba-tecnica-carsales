cb60b131e94428e03f7ddf1adc247b8e1485e81f
Javier Gutiérrez <jloravia@gmail.com>
2026-01-10 10:25:28 -0300

    feat: Añade provider HttpClient con Fetch por SSR. Corrige URL base en environment. Añade servicio characters. Ordena CharacterRoutes. Añade hidratación incremental en characters-list

--- src/app/features/characters/characters-routes.ts
+++ src/app/features/characters/characters-routes.ts
@@ -3,8 +3,6 @@ import { Routes } from '@angular/router';
 export const CharacterRoutes: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('./characters-list')
-        .then(c => c.CharactersList)
+    loadComponent: () => import('./characters-list').then(c => c.CharactersList)
   }
 ];
