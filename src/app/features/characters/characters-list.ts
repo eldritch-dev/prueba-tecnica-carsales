94be6bf9f80447276882066eaa5a0312de77d355
Javier Gutiérrez <jloravia@gmail.com>
2026-01-10 12:06:17 -0300

    feat: Añade librería UI Flowbite. Añade servicio de configuración de Flowbite. Importa estilos en archivo de estilos. Añade card potencialmente reutilizable. Implementa card en componente characters-list

--- src/app/features/characters/characters-list.ts
+++ src/app/features/characters/characters-list.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { Character } from './models/characters-model';
 import { CharactersService } from './service/characters-service';
+import { FbCard } from '../../shared/ui/fb-card';
 
 
 @Component({
   selector: 'app-characters-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FbCard],
   template: `
     <section>
       <h1 class="text-2xl font-semibold mb-4">Personajes de Rick y Morty</h1>
@@ -17,10 +18,7 @@ import { CharactersService } from './service/characters-service';
         @if (characters().length > 0) {
           <ul class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
             @for (char of characters(); track char.id) {
-              <li class="p-4 border rounded">
-                <h2 class="font-medium">{{ char.name }}</h2>
-                <p class="text-sm text-slate-600">{{ char.species }}</p>
-              </li>
+              <app-fb-card [data]="char"></app-fb-card>
             }
           </ul>
         } @else {
