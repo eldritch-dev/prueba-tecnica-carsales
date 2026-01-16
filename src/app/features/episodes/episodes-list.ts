2d5d7fb4f9853dc16fff841055fcfb1d0fcd2ffe
Javier Gutiérrez <jloravia@gmail.com>
2026-01-16 15:51:52 -0300

    fix: Añade Google Font. Añade más estilos globales. Modifica layout, character-cards, characters-list, episodes-listpara usar estilos globales propios del proyecto y no de Tailwind. Renombra servicio breakpoint para que coincida con nomenclatura de archivos. Corrige importaciones de BreakpointService

--- src/app/features/episodes/episodes-list.ts
+++ src/app/features/episodes/episodes-list.ts
@@ -1,4 +1,5 @@
 import { Component, computed, inject, Signal  } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 import { EpisodesService } from './service/episodes-service';
 import { Paginator } from '../../shared/paginator';
@@ -7,23 +8,24 @@ import { Episodes } from './models/episodes-model';
 import { ErrorService } from '../../shared/components/errors/service/error-service';
 import { ErrorMessage } from '../../shared/components/errors/error-message';
 import { TESButton } from '../../shared/tes-button';
+import { BreakpointService } from '../../services/breakpoint-service';
 
 @Component({
   selector: 'app-episodes-list',
   standalone: true,
-  imports: [ErrorMessage, Paginator, EpisodesCard, TESButton],
+  imports: [NgClass, ErrorMessage, Paginator, EpisodesCard, TESButton],
   template: `
     <section>
-      <div class="flex justify-between">
+      <div class="section-header" [ngClass]="{'flex-col': breakPointService.isSm()}">
         <h1 class="view-title bold-txt title-color">Rick y Morty Episodes</h1>
-        <app-tes-button (click)="triggerError()"></app-tes-button>
+        <app-tes-button (click)="triggerError()" [ngClass]="{'mb-4': breakPointService.isSm()}"></app-tes-button>
       </div>
       <app-error-message [isVisible]="errorService.isErrorVisible()" [error]="(errorService.error()?.error ?? '')" [traceId]="errorService.error()?.traceId"></app-error-message>
       @defer (hydrate on viewport) {
         @if (hasEpisodes()) {
-          <ul class="sm:flex sm:flex-col sm:justify-center place-items-center lg:gap-4 lg:grid lg:grid-cols-2 lg:justify-center">
+          <ul class="ptc-list">
             @for (episode of episodes().episodes; track episode.id) {
-              <app-episodes-card [data]="episode" class="w-full"></app-episodes-card>
+              <app-episodes-card [data]="episode" style="width: 100%;"></app-episodes-card>
             }
           </ul>
           <app-paginator (pageChange)="onPageChange($event)" [totalPages]="episodes().totalPages" [actualPage]="episodes().actualPage"></app-paginator>
@@ -40,6 +42,7 @@ import { TESButton } from '../../shared/tes-button';
 export class EpisodesList {
   episodesService = inject(EpisodesService);
   errorService = inject(ErrorService);
+  breakPointService = inject(BreakpointService);
 
   readonly episodes: Signal<Episodes> = this.episodesService.episodes;
 
